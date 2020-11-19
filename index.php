c8ddabe1a731a59cad7517bb3f778d8b68173581
= <=>
2020-11-19 18:31:18 +0530

    add header and footer

---
+++ index.php
@@ -0,0 +1,207 @@
+<?php include 'common\header.php'?>
+<section id="hero" class="d-flex align-items-center">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
+				<h1>Better Solutions For Your Business</h1>
+				<h2>We are team of talanted designers making websites with Bootstrap</h2>
+				<div class="d-lg-flex">
+					<a href="#about" class="btn-get-started scrollto">Get Started</a>
+					<a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
+				</div>
+			</div>
+			<div class="col-lg-6  order-1 order-lg-2">
+				<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/liquied.jpg" alt="sodium silicate liquid" width="800" height="300">
+        <div class="carousel-caption">
+        <h3 class="text-dark">liquid</h3>
+        <p class="text-dark">sodium silicate liquid</p>
+      </div> 
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/liquied1.jpg" alt="sodium silicate solid" width="800" height="300">
+       <div class="carousel-caption">
+        <h3 class="text-dark">liquid</h3>
+        <p class="text-dark">sodium silicate solid</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/liquied.jpg" alt="sodium meta silicate" width="800" height="300">
+       <div class="carousel-caption">
+        <h3 class="text-dark">liquid</h3>
+        <p class="text-dark">sodium meta silicate</p>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--about-->
+<section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About Us</h2>
+        </div>
+
+        <div class="row content">
+          <div class="col-lg-6">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+            </ul>
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <a href="#" class="btn-learn-more">Learn More</a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- -->
+ <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Our Product</h2>
+        </div>
+
+        <div class="row content">
+          <div class="col-sm-4 col-md-4 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+            <img alt="responsive" src="assets/img/liquied.jpg">
+                <h3>SODIUM  SILICATE SOLID</h3>
+                <p>Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+                         </div>
+          <div class="col-sm-4 col-md-4 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+            <img alt="responsive" src="assets/img/liquied.jpg">
+                <h3>SODIUM  SILICATE SOLID</h3>
+                <p>Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+                         </div>
+                              <div class="col-sm-4 col-md-4 feature_teaser" data-aos="zoom-in" data-aos-delay="100">
+            <img alt="responsive" src="assets/img/liquied.jpg">
+                <h3>SODIUM  SILICATE SOLID</h3>
+                <p>Sodium Silicate is one of most branched out and specialized product with its applications ranging to suit different products.<br><br><br></p>
+                         </div>
+        </div>
+
+      </div>
+    </section>
+
+    <!-- -->
+    <section id="pricing" class="pricing">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Company PRofile</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+      </div>
+    </section>
+
+
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-5 d-flex align-items-stretch">
+            <div class="info">
+              <div class="address">
+                <i class="icofont-google-map"></i>
+                <h4>Location:</h4>
+                <p>A108 Adam Street, New York, NY 535022</p>
+              </div>
+
+              <div class="email">
+                <i class="icofont-envelope"></i>
+                <h4>Email:</h4>
+                <p>info@example.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="icofont-phone"></i>
+                <h4>Call:</h4>
+                <p>+1 5589 55488 55s</p>
+              </div>
+
+             <iframe width="400" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=mehsana&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </div>
+
+          </div>
+
+          <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="name">Your Name</label>
+                  <input type="text" name="name" class="form-control" id="name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="name">Your Email</label>
+                  <input type="email" class="form-control" name="email" id="email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="name">Subject</label>
+                <input type="text" class="form-control" name="subject" id="subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <label for="name">Message</label>
+                <textarea class="form-control" name="message" rows="10" data-rule="required" data-msg="Please write something for us"></textarea>
+                <div class="validate"></div>
+              </div>
+              <div class="mb-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+<?php include 'common\footer.php'?>
